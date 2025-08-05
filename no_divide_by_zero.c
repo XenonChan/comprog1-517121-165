2934fb494dba25703e8e79d207f4d3fc7a78a5ae
Xenon <78194120+XenonChan@users.noreply.github.com>
2025-08-05 22:33:49 +0700

    lab_05/08/2025

--- no_divide_by_zero.c
+++ no_divide_by_zero.c
@@ -1,10 +1,13 @@
-//ยังทำบ่เส้ด
-
 #include<stdio.h>
 
 void main() {
     int z;
-    float x, y;
-    scanf("%f, %f", x, y);
-    scanf("%d")
+    double x, y;
+    scanf("%lf %lf %d", &x, &y, &z);
+    if (z == 0) {
+        printf("cannot divide by zero");
+    } else {
+
+        printf("%.6f", (x + y) / z);
+    }
 }
